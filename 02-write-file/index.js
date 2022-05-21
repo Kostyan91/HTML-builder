143342434c78a2ba2e6aabc9043fb98f28584e0f
Kostyan91 <kostyan912404@gmail.com>
2022-05-21 19:04:47 +0300

    feat:add task 2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,30 +1,38 @@
 const fs = require('fs');
 const path = require('path');
-const dirname = path.join(__dirname, 'text.txt');
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
-
-
-
 const rl = readline.createInterface({ input, output });
-function vvv(){
-//   let answer = '';
-  rl.question('What do you want to write? ', (answer) => {
-    function aaa() {
-      fs.appendFile(dirname, answer, function (error) {
-        if (error) throw error;
-        
-        // let data = fs.readFileSync(dirname, 'utf8');
-      });
-      
-    }
-    if (!answer.includes('exit')) {
-      aaa();
-      vvv();
-    } else { console.log('Thank you');
-      rl.close();
-    }
-      
-  });
+
+
+const fileName = path.join(__dirname, 'text.txt');
+
+// create file - text.txt
+fs.writeFile(fileName, "", (error) => {
+    if (error) throw error; // если возникла ошибка
+    console.log("File created.");
+});
+
+
+function readConsole() {
+    rl.question('', (answer) => {
+        if (answer.includes('exit')) {
+            console.log('Завершаем программу "exit".');
+            fs.appendFile(fileName, answer + '\n', (error) => {
+                if (error) throw error; // если возникла ошибка
+            });
+            rl.close();
+        } else {
+            fs.appendFile(fileName, answer + '\n', (error) => {
+                if (error) throw error; // если возникла ошибка
+                readConsole();
+            });
+        }
+    });         
 }
-vvv();
+
+readConsole();
+
+process.on('exit', (code) => {
+    console.log('Выходим из программы:', code);
+});
